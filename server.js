247ad4d294c57250756695993147ac81e64f2033
OmegaMan64 <sachever6@gmail.com>
2021-05-19 16:15:09 +0200

    commit

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/personal-portfoli-angular'));
+app.use(express.static('./dist/personal-portfolio-angular'));
 
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: './dist/personal-portfoli-angular' }
+  res.sendFile('index.html', { root: './dist/personal-portfolio-angular' }
   );
 });
 
