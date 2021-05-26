38f275d3a80968838f4e7926c47a5048b9dfb998
OmegaMan64 <sachever6@gmail.com>
2021-05-26 14:18:25 +0200

    deploy

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/personal-portfolio'));
+app.use(express.static('./dist/Personal-Portfolio'));
 
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: './dist/personal-portfolio' }
+  res.sendFile('index.html', { root: './dist/Personal-Portfolio' }
   );
 });
 
